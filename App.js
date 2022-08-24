427cab349f8354d45a018f24955038c6eb48703a
afrizalsofyan <afrizalafaandy@gmail.com>
2022-08-25 00:11:27 +0700

    fix: update create pin

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
 import Login from './src/screens/Login';
 import Register from './src/screens/Register';
+import CreatePin from './src/screens/CreatePin';
 
 const App = () => {
-  return <Register />;
+  return <CreatePin />;
 };
 
 export default App;
---
+++ src/screens/CreatePin.js
@@ -0,0 +1,110 @@
+import {View, Text, StatusBar, SafeAreaView, StyleSheet} from 'react-native';
+import React from 'react';
+import ReactNativePinView from 'react-native-pin-view';
+import Icon from 'react-native-vector-icons/Ionicons';
+import AuthLayout from '../components/layouts/AuthLayout';
+import HeaderAuthContent from '../components/HeaderAuthContent';
+
+const CreatePin = () => {
+  const pinView = React.useRef(null);
+  const [showRemoveButton, setShowRemoveButton] = React.useState(false);
+  const [enteredPin, setEnteredPin] = React.useState('');
+  const [showCompletedButton, setShowCompletedButton] = React.useState(false);
+  React.useEffect(() => {
+    if (enteredPin.length > 0) {
+      setShowRemoveButton(true);
+    } else {
+      setShowRemoveButton(false);
+    }
+    if (enteredPin.length === 6) {
+      setShowCompletedButton(true);
+    } else {
+      setShowCompletedButton(false);
+    }
+  }, [enteredPin]);
+  return (
+    <>
+      <AuthLayout
+        content={
+          <>
+            <HeaderAuthContent
+              title={'Create Security PIN'}
+              subtitle={
+                'Create a PIN that’s contain 6 digits number for security purpose in Our Pocket.'
+              }
+            />
+            <View style={style.wrapper}>
+              <ReactNativePinView
+                inputSize={32}
+                ref={pinView}
+                pinLength={6}
+                buttonSize={60}
+                onValueChange={value => setEnteredPin(value)}
+                inputAreaStyle={style.inputArea}
+                inputViewEmptyStyle={style.inputEmpty}
+                inputViewFilledStyle={style.inputFill}
+                buttonViewStyle={style.buttonStyle}
+                buttonTextStyle={style.buttonText}
+                onButtonPress={key => {
+                  if (key === 'custom_left') {
+                    pinView.current.clear();
+                  }
+                  if (key === 'custom_right') {
+                    alert('Entered Pin: ' + enteredPin);
+                  }
+                  if (key === 'three') {
+                    alert("You can't use 3");
+                  }
+                }}
+                customLeftButton={
+                  showRemoveButton ? (
+                    <Icon
+                      name={'ios-backspace-outline'}
+                      size={42}
+                      color={'black'}
+                    />
+                  ) : undefined
+                }
+                customRightButton={
+                  showCompletedButton ? (
+                    <Icon
+                      name={'ios-checkmark-circle-outline'}
+                      size={42}
+                      color={'black'}
+                    />
+                  ) : undefined
+                }
+              />
+            </View>
+          </>
+        }
+      />
+    </>
+  );
+};
+
+const style = StyleSheet.create({
+  wrapper: {
+    marginBottom: 25,
+  },
+  inputEmpty: {
+    backgroundColor: 'transparent',
+    borderWidth: 1,
+    borderColor: 'black',
+  },
+  inputFill: {
+    backgroundColor: 'black',
+  },
+  buttonStyle: {
+    borderWidth: 1,
+    borderColor: 'black',
+  },
+  buttonText: {
+    color: 'black',
+  },
+  inputArea: {
+    marginBottom: 24,
+  }
+});
+
+export default CreatePin;
