25b6cc9f58055e819596255d9b98e02e96a70e2b
afrizalsofyan <afrizalafaandy@gmail.com>
2022-09-05 17:26:12 +0700

    fix: add icon

--- App.js
+++ App.js
@@ -5,19 +5,26 @@ import {PersistGate} from 'redux-persist/integration/react';
 import NetInfo, {useNetInfo} from '@react-native-community/netinfo';
 import NoConnectionInternet from './src/components/NoConnectionInternet';
 import AuthStack from './src/screens/AuthStack';
+import RNBootSplash from 'react-native-bootsplash';
 
 const App = () => {
   const netInfo = useNetInfo();
   const [online, setOnline] = React.useState(true);
   React.useEffect(() => {
-    NetInfo.fetch().then(state => {
-      console.log('Connection type', state.type);
-      console.log('Is connected?', state.isConnected);
-      if (state.isConnected) {
-        setOnline(true);
-      } else {
-        setOnline(false);
-      }
+    const init = async () => {
+      NetInfo.fetch().then(state => {
+        console.log('Connection type', state.type);
+        console.log('Is connected?', state.isConnected);
+        if (state.isConnected) {
+          setOnline(true);
+        } else {
+          setOnline(false);
+        }
+      });
+    };
+    init().finally(async () => {
+      await RNBootSplash.hide({fade: true});
+      console.log('Splash screen hidden');
     });
   }, []);
 
